a81f650db6ed297e906f959604131c67a816742d
Abed.Matini <abed.matini@ogilvy.co.za>
2024-06-30 12:15:28 +0200

    created functional react component, render child component from paretn component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
 import Tweet from "@/Tweet";
+import path from "path";
+import fs from "fs";
 
-export default function Home(){
+export default function Home( {tweets} ){
   return (
     <div className="{styles.container}">
       <h1>Hello, world!</h1>
-      <Tweet text="passed down prop" author='Abed' date='30/06' />
+      {tweets.map(tweet => <Tweet Key = {tweet.id} text={tweet.text} author={tweet.author} data={tweet.date} />)}
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const filepath1 = path.join(process.cwd(), 'data.json')
+  const tweets1 = JSON.parse(fs.readFileSync(filepath1))
+  const filepath2 = path.join(process.cwd(), 'data2.json')
+  const tweets2 = JSON.parse(fs.readFileSync(filepath2))
+
+  const tweets = tweets1.concat(tweets2)
+  return {
+    props: {
+      tweets
+    }
+  }
+  
 }
