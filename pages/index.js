6306aa69077d5016787268b227aebf5d298e69ac
Abed.Matini <abed.matini@ogilvy.co.za>
2024-06-30 10:58:33 +0200

    hello world!

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+import Head from "next/head"
+
+export default function Home(){
+  return (
+    <div className="{styles.container}">
+<h1>Hello, world!</h1>
+      </div>
+  )
+}
